6329db4a8437bbe21997293605a9ff695b7b2994
kevpro95 <k.triebnig@student.tugraz.at>
2018-04-09 10:07:21 +0200

    [KT, PO] added registry activity

--- app/src/main/java/com/educationaid/tutoring/HomeActivity.java
+++ app/src/main/java/com/educationaid/tutoring/HomeActivity.java
@@ -16,7 +16,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            
+
         }
     }
 }
