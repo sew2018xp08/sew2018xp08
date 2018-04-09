3ac7f2a0cc6d15b5b0547fede27ec2e668507435
kevpro95 <k.triebnig@student.tugraz.at>
2018-04-09 12:06:14 +0200

    [KT, OP] textview validation

--- app/src/main/java/com/educationaid/tutoring/RegistryActivity.java
+++ app/src/main/java/com/educationaid/tutoring/RegistryActivity.java
@@ -2,12 +2,45 @@ package com.educationaid.tutoring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegistryActivity extends AppCompatActivity {
+public class RegistryActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button registry_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
+         (findViewById(R.id.btnRegistry)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnRegistry)
+            registryValidation();
+    }
+
+    private void registryValidation() {
+        if(((TextView)findViewById(R.id.txtNameRegistry)).getText().equals(""))
+            ((TextView)findViewById(R.id.lblErrorNameRegistry)).setText("Darf nicht leer sein!");
+
+        if(((TextView)findViewById(R.id.txtSurnameRegistry)).getText().equals(""))
+            ((TextView)findViewById(R.id.lblErrorSurnameRegistry)).setText("Darf nicht leer sein!");
+
+        if(((TextView)findViewById(R.id.txtEmailRegistry)).getText().equals(""))
+            ((TextView)findViewById(R.id.lblErrorEmailRegistry)).setText("Darf nicht leer sein!");
+
+        if(((TextView)findViewById(R.id.txtPasswordRegistry)).getText().equals(""))
+            ((TextView)findViewById(R.id.lblErrorPasswordRegistry)).setText("Darf nicht leer sein!");
+
+        if(((TextView)findViewById(R.id.txtPasswordconfRegistry)).getText().equals(""))
+            ((TextView)findViewById(R.id.lblErrorPasswordconfRegistry)).setText("Darf nicht leer sein!");
+
+        if(!(((TextView)findViewById(R.id.txtPasswordRegistry)).getText().equals(((TextView)findViewById(R.id.txtPasswordconfRegistry)).getText()))){
+            ((TextView)findViewById(R.id.lblErrorPasswordconfRegistry)).setText("Passwörter stimmen nicht überein!");
+            ((TextView)findViewById(R.id.lblErrorPasswordRegistry)).setText("Passwörter stimmen nicht überein!");
+        }
     }
 }
