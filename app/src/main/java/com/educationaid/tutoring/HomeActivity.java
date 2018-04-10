63c07e7d8c117e9ffedccdd9f6112c051afcd0ab
Michael Weiß <michael.weiss@student.tugraz.at>
2018-04-10 17:47:19 +0200

    [MW, JR] Basic Login Logic added

--- app/src/main/java/com/educationaid/tutoring/HomeActivity.java
+++ app/src/main/java/com/educationaid/tutoring/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.educationaid.tutoring;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent to_login = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(to_login);
     }
 
     @Override
--- app/src/main/java/com/educationaid/tutoring/LoginActivity.java
+++ app/src/main/java/com/educationaid/tutoring/LoginActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView txt_username;
-    TextView txt_password;
+    EditText txt_username;
+    EditText txt_password;
+    TextView txt_warning;
     Button btn_login;
     Button btn_register;
 
@@ -20,12 +24,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
 
-        txt_username = (TextView) findViewById(R.id.txtUserName);
-        txt_password = (TextView) findViewById(R.id.txtPassword);
+        txt_username = (EditText) findViewById(R.id.txtUserName);
+        txt_password = (EditText) findViewById(R.id.txtPassword);
+        txt_warning = (TextView) findViewById(R.id.txtWarning);
         btn_login = (Button) findViewById(R.id.btnLogin);
         btn_login.setOnClickListener(this);
         btn_register = (Button) findViewById(R.id.btnRegister);
         btn_register.setOnClickListener(this);
+        txt_warning.setVisibility(View.INVISIBLE);
+
 
     }
 
@@ -34,6 +41,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Button clickedButton = (Button) v;
         switch (clickedButton.getId()) {
             case R.id.btnLogin:
+                if(txt_username.getText().equals("") || txt_password.getText().equals(""))
+                {
+                    //todo: Create string class..
+                    txt_warning.setVisibility(View.VISIBLE);
+                }
                 //do something
                 break;
             case R.id.btnRegister:
