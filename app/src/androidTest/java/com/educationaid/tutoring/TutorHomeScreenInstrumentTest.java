0e34572657ccc8d76109b4f1801e06878b9ef7d1
Fabian Hofmann <fabian.hofmann@student.tugraz.at>
2018-04-11 14:01:30 +0200

    [MK, FH] test for tutor home view

---
+++ app/src/androidTest/java/com/educationaid/tutoring/TutorHomeScreenInstrumentTest.java
@@ -0,0 +1,37 @@
+package com.educationaid.tutoring;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by fabianhofmann on 11.04.18.
+ */
+
+public class TutorHomeScreenInstrumentTest {
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.educationaid.tutoring", appContext.getPackageName());
+    }
+    @Test
+    public void useLogoutButton() throws  Exception {
+        onView(withText("Logout")).perform(click());
+    }
+    @Test
+    public void TestIsPresent()throws  Exception {
+        onView(withText("Hello Max Mustermann")).check(matches(isDisplayed()));
+    }
+}
