70c3f8dae666c5bf2dc864c3852043b3eba25893
Martin.K <martin.k@usr.schoste.com>
2018-04-17 23:21:03 +0200

    [MK, FH] added floating button instead of button in title bar and changed some layout settings

--- app/src/main/java/com/educationaid/tutoring/TutorHomeScreenActivity.java
+++ app/src/main/java/com/educationaid/tutoring/TutorHomeScreenActivity.java
@@ -1,6 +1,7 @@
 package com.educationaid.tutoring;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -8,16 +9,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ListView;
 
 import com.educationaid.tutoring.adapters.OfferListAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class TutorHomeScreenActivity extends AppCompatActivity {
 
@@ -49,7 +46,7 @@ public class TutorHomeScreenActivity extends AppCompatActivity {
             }
         });
 
-        Button btnAdd = findViewById(R.id.buttonAdd);
+        FloatingActionButton btnAdd = findViewById(R.id.buttonAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(TutorHomeScreenActivity.this, CreateOfferActivity.class);
