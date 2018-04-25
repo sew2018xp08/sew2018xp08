f4724f0ba2c98a55295fca4541ad34ba3763f304
Patrick Schwarz <patrick.schwarz@student.tugraz.at>
2018-04-25 16:08:25 +0200

    WebserviceClass und Test

---
+++ app/src/test/java/com/educationaid/tutoring/PHPWebserviceTest.java
@@ -0,0 +1,25 @@
+package com.educationaid.tutoring;
+
+import com.educationaid.tutoring.Constants.Constants;
+import com.educationaid.tutoring.WebService.WebService;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class PHPWebserviceTest {
+    @Test
+    public void PHPLogin() throws Exception {
+        String result = WebService.Login("john@example.com", "1234");
+        if(result.equals(Constants.ANS_RIGHT_USERNAME_PASSWORD)){
+            System.out.println("HTTP POST is working...");
+        }else{
+            System.out.println("Invalid POST req...");
+        }
+    }
+}
