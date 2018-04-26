a2d4733753e15ca14c8fbe67659f567c673155d6
Patrick Schwarz <patrick.schwarz@student.tugraz.at>
2018-04-26 16:39:36 +0200

    Webservice Tested, Excluded Webservice Functions out of AndroidTests Dependecies changed

--- app/src/androidTest/java/com/educationaid/tutoring/RegisterInstrumentedTest.java
+++ app/src/androidTest/java/com/educationaid/tutoring/RegisterInstrumentedTest.java
@@ -1,31 +1,16 @@
 package com.educationaid.tutoring;
 
-import android.os.AsyncTask;
-import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.educationaid.tutoring.Constants.Constants;
+import com.educationaid.tutoring.WebService.WebService;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import junit.framework.Assert;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
@@ -49,7 +34,7 @@ public class RegisterInstrumentedTest {
 
     @Test
     public void testRegister() {
-        deleteTestUserFromDataBase();
+        Assert.assertTrue(WebService.DeleteTestUserFromDataBase().equals(Constants.ANS_DELETE_USER));
         onView(withId(R.id.txtNameRegistry)).perform(scrollTo(), typeText("Max"));
         onView(withId(R.id.txtSurnameRegistry)).perform(scrollTo(), typeText("Mustermann"));
         onView(withId(R.id.txtEmailRegistry)).perform(scrollTo(), typeText("test@test.com"));
@@ -66,13 +51,13 @@ public class RegisterInstrumentedTest {
         onView(withId(R.id.btnRegistry)).perform(scrollTo(), click());
         onView(withText(R.string.successful_registry)).inRoot(new ToastMatcher())
                 .check(matches(isDisplayed()));
-        deleteTestUserFromDataBase();
+        Assert.assertTrue(WebService.DeleteTestUserFromDataBase().equals(Constants.ANS_DELETE_USER));
     }
 
 
     @Test
     public void testRegisterExistingUser() {
-        deleteTestUserFromDataBase();
+        Assert.assertTrue(WebService.DeleteTestUserFromDataBase().equals(Constants.ANS_DELETE_USER));
         onView(withId(R.id.txtNameRegistry)).perform(scrollTo(), typeText("Max"));
         onView(withId(R.id.txtSurnameRegistry)).perform(scrollTo(), typeText("Mustermann"));
         onView(withId(R.id.txtEmailRegistry)).perform(scrollTo(), typeText("test@test.com"));
@@ -107,68 +92,9 @@ public class RegisterInstrumentedTest {
         onView(withId(R.id.btnRegistry)).perform(scrollTo(), click());
         onView(withText(R.string.user_alredy_exists)).inRoot(new ToastMatcher())
                 .check(matches(isDisplayed()));
-        deleteTestUserFromDataBase();
+        Assert.assertTrue(WebService.DeleteTestUserFromDataBase().equals(Constants.ANS_DELETE_USER));
     }
 
 
-    public static void deleteTestUserFromDataBase() {
-        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
 
-            @Override
-            protected String doInBackground(String... params) {
-                HttpClient httpClient = new DefaultHttpClient();
-
-                // In a POST request, we don't pass the values in the URL.
-                //Therefore we use only the web page URL as the parameter of the HttpPost argument
-                HttpPost httpPost = new HttpPost(Constants.PHP_TEST_DELETE_USER);
-                    try {
-                        // HttpResponse is an interface just like HttpPost.
-                        //Therefore we can't initialize them
-                        HttpResponse httpResponse = httpClient.execute(httpPost);
-
-                        // According to the JAVA API, InputStream constructor do nothing.
-                        //So we can't initialize InputStream although it is not an interface
-                        InputStream inputStream = httpResponse.getEntity().getContent();
-
-                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                        StringBuilder stringBuilder = new StringBuilder();
-
-                        String bufferedStrChunk = null;
-
-                        while((bufferedStrChunk = bufferedReader.readLine()) != null){
-                            stringBuilder.append(bufferedStrChunk);
-                        }
-
-                        return stringBuilder.toString();
-
-                    } catch (ClientProtocolException cpe) {
-                        System.out.println("First Exception caz of HttpResponese :" + cpe);
-                        cpe.printStackTrace();
-                    } catch (IOException ioe) {
-                        System.out.println("Second Exception caz of HttpResponse :" + ioe);
-                        ioe.printStackTrace();
-                    }
-
-
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-
-                if(result.equals(Constants.ANS_DELETE_USER)){
-                    System.out.println("HTTP POST is working...");
-                }else{
-                    System.out.println("Invalid POST req...");
-                }
-            }
-        }
-
-        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
-        sendPostReqAsyncTask.execute();
-    }
 }
