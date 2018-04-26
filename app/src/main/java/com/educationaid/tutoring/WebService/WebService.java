0b16bad7a268e270ae1bd3ef84d1b440ffbf4383
Patrick Schwarz <patrick.schwarz@student.tugraz.at>
2018-04-26 16:39:36 +0200

    Webservice Tested, Excluded Webservice Functions out of AndroidTests Dependecies changed

--- app/src/main/java/com/educationaid/tutoring/WebService/WebService.java
+++ app/src/main/java/com/educationaid/tutoring/WebService/WebService.java
@@ -10,6 +10,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,6 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WebService  {
@@ -24,56 +26,17 @@ public class WebService  {
     public static String Login(String paramEmail, String paramPassword) {
 
         HttpClient httpClient = new DefaultHttpClient();
-
-        // In a POST request, we don't pass the values in the URL.
-        //Therefore we use only the web page URL as the parameter of the HttpPost argument
         HttpPost httpPost = new HttpPost(Constants.PHP_LOGIN_LIMK);
 
-        // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
-        //uniquely separate by the other end.
-        //To achieve that we use BasicNameValuePair
-        //Things we need to pass with the POST request
-        BasicNameValuePair emailBasicNameValuePair = new BasicNameValuePair(Constants.POST_ID_EMAIL, paramEmail);
-        BasicNameValuePair passwordBasicNameValuePAir = new BasicNameValuePair(Constants.POST_ID_PASSWORD, paramPassword);
-
-        // We add the content that we want to pass with the POST request to as name-value pairs
-        //Now we put those sending details to an ArrayList with type safe of NameValuePair
-        List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
-        nameValuePairList.add(emailBasicNameValuePair);
-        nameValuePairList.add(passwordBasicNameValuePAir);
-
         try
-
         {
-            // UrlEncodedFormEntity is an entity composed of a list of url-encoded pairs.
-            //This is typically useful while sending an HTTP POST request.
-            UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairList);
-
-            // setEntity() hands the entity (here it is urlEncodedFormEntity) to the request.
-            httpPost.setEntity(urlEncodedFormEntity);
+            httpPost.setEntity(new UrlEncodedFormEntity(Arrays.asList(
+                    new BasicNameValuePair(Constants.POST_ID_EMAIL, paramEmail),
+                    new BasicNameValuePair(Constants.POST_ID_PASSWORD, paramPassword)
+            )));
 
             try {
-                // HttpResponse is an interface just like HttpPost.
-                //Therefore we can't initialize them
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-
-                // According to the JAVA API, InputStream constructor do nothing.
-                //So we can't initialize InputStream although it is not an interface
-                InputStream inputStream = httpResponse.getEntity().getContent();
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder stringBuilder = new StringBuilder();
-
-                String bufferedStrChunk = null;
-
-                while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(bufferedStrChunk);
-                }
-
-                return stringBuilder.toString();
+                return EntityUtils.toString(httpClient.execute(httpPost).getEntity(), "UTF-8");
 
             } catch (ClientProtocolException cpe) {
                 System.out.println("First Exception caz of HttpResponese :" + cpe);
@@ -135,6 +98,24 @@ public class WebService  {
             System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + uee);
             uee.printStackTrace();
         }
+        return null;
+    }
+
+    public static String DeleteTestUserFromDataBase() {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(Constants.PHP_TEST_DELETE_USER);
+        try {
+            return EntityUtils.toString(httpClient.execute(httpPost).getEntity(), "UTF-8");
+
+        } catch (ClientProtocolException cpe) {
+            System.out.println("First Exception caz of HttpResponese :" + cpe);
+            cpe.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("Second Exception caz of HttpResponse :" + ioe);
+            ioe.printStackTrace();
+        }
+
+
         return null;
     }
 }
