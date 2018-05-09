c4041bc5e458273cae797997dc5860f348d18303
tamara <tamara.steinwender@gmx.t>
2018-05-09 15:00:11 +0200

    [KT, TS] update new_offer.php

--- php/new_offer.php
+++ php/new_offer.php
@@ -1,37 +1,53 @@
 <?php
 
-    include "/users/sew2018xp08/www/PHP/config.php";
+include "/users/sew2018xp08/www/PHP/config.php";
 
-    $u_id = $_POST['tutorID'];
-    $title = $_POST['title'];
-    $desc = $_POST['description'];
+$u_id = $_POST['tutorID'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+$price = $_POST['price'];
 
-    //title + user exists already?
-    $sql = "SELECT * FROM offers WHERE u_id = '$u_id' and title = '$title'";
 
-    $result = $conn->query($sql);
+//title + user exists already?
+$sql = 'SELECT * FROM offers WHERE u_id = ? and title = ?';
 
-    if ($result->num_rows > 0)
+/* prepare statement */
+if ($stmt = $conn->prepare($sql))
+{
+    $stmt->bind_param("is", $u_id, $title);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result)
     {
-        echo "Eintrag vom user ".$u_id." und titel ".$title." gibts schu";
-        
-    }
-    else
-    {
-        //Insert
-        $sql = "INSERT INTO offers (u_id, title, description)
-        VALUES ('$u_id', '$title', '$desc')";
-
-        //check succsess
-        if ($conn->query($sql) === TRUE)
+        if ($result->num_rows > 0)
         {
-            echo "New offer created successfully";
+            echo "Eintrag vom user ".$u_id." und titel ".$title." gibts schu";
         }
         else
         {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            //Insert
+            $sql2 = 'INSERT INTO offers (u_id, title, description, price)
+                      VALUES (?, ?, ?, ?)';
+
+            /* prepare statement */
+            if ($stmt2 = $conn->prepare($sql2))
+            {
+                $stmt2->bind_param("iss", $u_id, $title, $desc, $price);
+
+                if ($stmt2->execute())
+                {
+                    echo "New offer created successfully";
+                }
+                else
+                {
+                    echo "Error: " . $sql2 . "<br>" . $conn->error;
+                }
+                $stmt2->close();
+            }
         }
     }
-
-    $conn->close();
+    $stmt->close();
+}
+$conn->close();
 ?>
