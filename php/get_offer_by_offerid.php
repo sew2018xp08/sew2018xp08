b5053b776217e1c5abd79c449b34074eb5c2d9e2
tamara <tamara.steinwender@gmx.t>
2018-05-23 13:59:42 +0200

    [ST, WM] Update PHPs

---
+++ php/get_offer_by_offerid.php
@@ -0,0 +1,35 @@
+<?php
+    include "/users/sew2018xp08/www/PHP/config.php";
+
+    $o_id = $_POST['offerID'];
+    $sql = 'SELECT * FROM offers o, user u WHERE o.o_id = ? and o.u_id = u.u_id';
+    $user_data = array();
+
+    /* prepare statement */
+
+    if ($stmt = $conn->prepare($sql))
+
+    {
+
+        $stmt->bind_param("i", $o_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result)
+        {
+            if ($result->num_rows > 0)
+            {
+                while($row = $result->fetch_assoc())
+                {
+                    $user_data[] = $row;
+                }
+
+                echo json_encode($user_data, JSON_UNESCAPED_UNICODE);
+            } else {
+                echo "NO OFFERS HERE";
+            }
+        }
+        $stmt->close();
+    }
+    $conn->close();
+?>
