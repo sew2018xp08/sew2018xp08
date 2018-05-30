ca37d4dae36e815d25df7733edc57327b064dd9f
Martin.K <>
2018-05-30 16:48:40 +0200

    [MK, KT] layout changes of detail offer view

--- app/src/main/java/com/educationaid/tutoring/OfferDetailActivity.java
+++ app/src/main/java/com/educationaid/tutoring/OfferDetailActivity.java
@@ -1,12 +1,17 @@
 package com.educationaid.tutoring;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Base64;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.educationaid.tutoring.Constants.Constants;
@@ -35,6 +40,7 @@ public class OfferDetailActivity extends AppCompatActivity implements View.OnCli
     private String email;
     private String price;
     private String title;
+    private boolean isProOffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +49,12 @@ public class OfferDetailActivity extends AppCompatActivity implements View.OnCli
         Bundle bundle = new Bundle();
         bundle = getIntent().getExtras();
         int parentId = bundle.getInt("view");
+
+        TextView description = findViewById(R.id.txtDescriptionDetail);
+        description.setMovementMethod(new ScrollingMovementMethod());
+
+        if (HomeActivity.currentUser.getProfilePicture() != null)
+            ((ImageView) findViewById(R.id.imgTutor)).setImageBitmap(HomeActivity.currentUser.getProfilePicture());
 
         ((Button)findViewById(R.id.ButtonContactDetail)).setOnClickListener(this);
         getOffer(String.valueOf(bundle.getInt("offerid")));
@@ -57,13 +69,31 @@ public class OfferDetailActivity extends AppCompatActivity implements View.OnCli
         }
         return super.onOptionsItemSelected(item);
     }
+
     private void fillTextViews(JSONArray obj) throws JSONException {
         ((TextView)findViewById(R.id.txtTitleDetail)).setText(obj.getJSONObject(0).getString("title"));
         ((TextView)findViewById(R.id.txtDescriptionDetail)).setText(obj.getJSONObject(0).getString("description"));
-        ((TextView)findViewById(R.id.textViewPriceDetail)).setText(obj.getJSONObject(0).getString("price") + " / Hour");
+        ((TextView) findViewById(R.id.textViewPriceDetail)).setText(obj.getJSONObject(0).getString("price") + " € / Hour");
+        ((TextView) findViewById(R.id.tutorName)).setText(obj.getJSONObject(0).getString("first_name") + " " +
+                obj.getJSONObject(0).getString("last_name"));
+
+
         email = obj.getJSONObject(0).getString("email");
         price = obj.getJSONObject(0).getString("price");
         title = obj.getJSONObject(0).getString("title");
+        isProOffer = obj.getJSONObject(0).getString("pro").equals("0") ? false : true;
+        if (isProOffer) setPicture(obj.getJSONObject(0).getString("profilePicture"));
+    }
+
+    private void setPicture(String picture) {
+        String encodedImage = picture;
+        Bitmap decodedByte = null;
+
+        if (!encodedImage.equals("null")) {
+            byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+            decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            ((ImageView) findViewById(R.id.imgTutor)).setImageBitmap(decodedByte);
+        }
     }
 
     public void getOffer(String offerid) {
